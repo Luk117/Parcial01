5fd9f6a3bbd8b6356400e874fd0e8d28c61cd09e
Lukas Nieto <lukasnieto117@gmail.com>
2021-03-05 19:34:41 -0500

    INICIO

--- Parcial01/src/com/Luk117/Group.java
+++ Parcial01/src/com/Luk117/Group.java
@@ -1,5 +1,20 @@
 package com.Luk117;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+
+    private String name;
+    private List<Project> projects;
+
+    public Group(String name) {
+        this.name = name;
+        this.projects = new ArrayList<>();
+    }
+
+    public void addProject(Project plan) {
+        this.projects.add(plan);
+    }
 
 }
