40ecb472ec74c3ee7aaa63620d3b6d5b0adb4057
Lukas Nieto <lukasnieto117@gmail.com>
2021-03-05 19:33:35 -0500

    INICIO

---
+++ Parcial01/src/com/Luk117/SabanaResearch.java
@@ -0,0 +1,23 @@
+package com.Luk117;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SabanaResearch {
+
+    private List<Group> groups;
+    private List<Summary> summaries;
+
+    public SabanaResearch(List<Group> groups) {
+        this.groups = groups;
+        this.summaries = new ArrayList<>();
+    }
+
+    public int countOfGroups() {
+        return this.groups.size();
+    }
+
+    public int countOfSummaries() {
+        return this.summaries.size();
+    }
+}
---
+++ Parcial01/src/com/Luk117/Summary.java
@@ -0,0 +1,17 @@
+package com.Luk117;
+
+import java.time.LocalDate;
+
+public class Summary {
+    private int activeProjects;
+    private LocalDate date;
+
+
+    public int getActiveProjects() {
+        return activeProjects;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
